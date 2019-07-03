4ad92f4d77a74113c88faf69a3fdc44e8dd180c4
Fahmi Agung Tajul Abidin <fata.22032001@gmail.com>
2019-07-03 15:02:22 +0700

    fix Change port server

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  var port_number = server.listen(process.env.PORT || 3000);
-  await app.listen(port_number);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
